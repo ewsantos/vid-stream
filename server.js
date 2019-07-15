08cd659418392485f9f80b5e58aa2eb94728336f
ewsantos <22549387+ewsantos@users.noreply.github.com>
2019-07-15 10:06:43 -0700

    get dynamic mp4 file name

--- server.js
+++ server.js
@@ -3,43 +3,49 @@ const fs = require('fs')
 const path = require('path')
 const app = express()
 
-app.use(express.static(path.join(__dirname, 'public/static')))
+app.use(express.static('public/static'));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/public/pages/home/home.html'))
 })
 
-app.get('/video', function(req, res) {
-  const path = 'assets/sample.mp4'
-  const stat = fs.statSync(path)
-  const fileSize = stat.size
-  const range = req.headers.range
+app.get('/video/:vid', function(req, res) {
+  var ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
+  console.log(ip);
+  try {
+    const path = "assets/" + req.params.vid
+    const stat = fs.statSync(path)
+    const fileSize = stat.size
+    const range = req.headers.range
 
-  if (range) {
-    const parts = range.replace(/bytes=/, "").split("-")
-    const start = parseInt(parts[0], 10)
-    const end = parts[1]
-      ? parseInt(parts[1], 10)
-      : fileSize-1
+    if (range) {
+      const parts = range.replace(/bytes=/, "").split("-")
+      const start = parseInt(parts[0], 10)
+      const end = parts[1]
+        ? parseInt(parts[1], 10)
+        : fileSize-1
 
-    const chunksize = (end-start)+1
-    const file = fs.createReadStream(path, {start, end})
-    const head = {
-      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-      'Accept-Ranges': 'bytes',
-      'Content-Length': chunksize,
-      'Content-Type': 'video/mp4',
+      const chunksize = (end-start)+1
+      const file = fs.createReadStream(path, {start, end})
+      const head = {
+        'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+        'Accept-Ranges': 'bytes',
+        'Content-Length': chunksize,
+        'Content-Type': 'video/mp4',
+      }
+
+      res.writeHead(206, head)
+      file.pipe(res)
+    } else {
+      const head = {
+        'Content-Length': fileSize,
+        'Content-Type': 'video/mp4',
+      }
+      res.writeHead(200, head)
+      fs.createReadStream(path).pipe(res)
     }
-
-    res.writeHead(206, head)
-    file.pipe(res)
-  } else {
-    const head = {
-      'Content-Length': fileSize,
-      'Content-Type': 'video/mp4',
-    }
-    res.writeHead(200, head)
-    fs.createReadStream(path).pipe(res)
+  } catch (e) {
+      console.error(e);
   }
 })
 
