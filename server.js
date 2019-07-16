942305a4e13c89ff77c2bc975aab2ebc7b7b20e0
ewsantos <22549387+ewsantos@users.noreply.github.com>
2019-07-15 23:08:05 -0700

    commit

--- server.js
+++ server.js
@@ -13,36 +13,39 @@ app.get('/video/:vid', function(req, res) {
   var ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
   console.log(ip);
   try {
-    const path = "assets/" + req.params.vid
-    const stat = fs.statSync(path)
-    const fileSize = stat.size
-    const range = req.headers.range
+    if (typeof req.params.vid == 'string') {
+      const path = "assets/" + req.params.vid
+      console.log(path);
+      const stat = fs.statSync(path)
+      const fileSize = stat.size
+      const range = req.headers.range
 
-    if (range) {
-      const parts = range.replace(/bytes=/, "").split("-")
-      const start = parseInt(parts[0], 10)
-      const end = parts[1]
-        ? parseInt(parts[1], 10)
-        : fileSize-1
+      if (range) {
+        const parts = range.replace(/bytes=/, "").split("-")
+        const start = parseInt(parts[0], 10)
+        const end = parts[1]
+          ? parseInt(parts[1], 10)
+          : fileSize-1
 
-      const chunksize = (end-start)+1
-      const file = fs.createReadStream(path, {start, end})
-      const head = {
-        'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-        'Accept-Ranges': 'bytes',
-        'Content-Length': chunksize,
-        'Content-Type': 'video/mp4',
+        const chunksize = (end-start)+1
+        const file = fs.createReadStream(path, {start, end})
+        const head = {
+          'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+          'Accept-Ranges': 'bytes',
+          'Content-Length': chunksize,
+          'Content-Type': 'video/mp4',
+        }
+
+        res.writeHead(206, head)
+        file.pipe(res)
+      } else {
+        const head = {
+          'Content-Length': fileSize,
+          'Content-Type': 'video/mp4',
+        }
+        res.writeHead(200, head)
+        fs.createReadStream(path).pipe(res)
       }
-
-      res.writeHead(206, head)
-      file.pipe(res)
-    } else {
-      const head = {
-        'Content-Length': fileSize,
-        'Content-Type': 'video/mp4',
-      }
-      res.writeHead(200, head)
-      fs.createReadStream(path).pipe(res)
     }
   } catch (e) {
       console.error(e);
